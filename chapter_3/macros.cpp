3a1b5c4921cc0e1d0f8312e9025db481179486a4
Stephen Schor <stephen@eastmedia.com>
2013-08-09 11:59:26 -0400

    macro example

---
+++ chapter_3/macros.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+
+#define prettyPrint(v) cout << v << endl;
+#define five 5
+
+int main (int argc, char const *argv[])
+{
+  prettyPrint("I Feel Pretty");
+  prettyPrint("oh so pretty");
+  cout << "Five plus 10 = " << (five + 10) << endl;
+  return 0;
+}
+
